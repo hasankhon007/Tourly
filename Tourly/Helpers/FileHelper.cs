e7a5cf8d9aab33dfca46649e092d9dbb91624719
hasankhon007 <savriddinovhasanxon007@gmail.com>
2025-07-13 14:44:30 +0500

    Helper

--- Tourly/Helpers/FileHelper.cs
+++ Tourly/Helpers/FileHelper.cs
@@ -2,4 +2,18 @@
 
 public class FileHelper
 {
+    public static string ReadFromFile(string filePath)
+    {
+        if (!File.Exists(filePath))
+        {
+            File.Create(filePath).Close(); // Ensure the file exists
+        }
+
+        return File.ReadAllText(filePath);
+    }
+
+    public static void WriteToFile(string filePath, List<string> content)
+    {
+        File.WriteAllLines(filePath, content);
+    }
 }
