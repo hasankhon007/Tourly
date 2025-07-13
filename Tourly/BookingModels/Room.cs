f09f5fbb219ecb019d737e9e78851005b6883bb1
Ravshan04 <rn3925471@gmail.com>
2025-07-13 15:23:40 +0500

    I added models for booking

---
+++ Tourly/BookingModels/Room.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tourly.Enums;
+
+namespace Tourly.BookingModels
+{
+    public class Room
+    {
+        public Guid Id { get; set; }
+        public string RoomNumber { get; set; }       
+        public RoomType RoomType { get; set; }         
+        public int Capacity { get; set; }            
+        public RoomStatus Status { get; set; }       
+        public decimal PricePerNight { get; set; }   
+
+        public override string ToString()
+        {
+            return $"{Id}|{RoomNumber}|{RoomType}|{Capacity}|{Status}|{PricePerNight}";
+        }
+
+        public static Room FromString(string line)
+        {
+            var parts = line.Split('|');
+            return new Room
+            {
+                Id = Guid.Parse(parts[0]),
+                RoomNumber = parts[1],
+                RoomType = (RoomType)Enum.Parse(typeof(RoomType), parts[2]),
+                Capacity = int.Parse(parts[3]),
+                Status = Enum.Parse<RoomStatus>(parts[4]),
+                PricePerNight = decimal.Parse(parts[5])
+            };
+        }
+    }
+}
