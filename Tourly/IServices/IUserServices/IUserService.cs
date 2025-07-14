74cf6dba9c9d895aeac59d4b0bfce4ab3d06b5d1
hasankhon <savriddinovhasanxon007@gmail.com>
2025-07-14 12:09:17 +0500

    User services

--- Tourly/IServices/IUserServices/IUserService.cs
+++ Tourly/IServices/IUserServices/IUserService.cs
@@ -1,5 +1,19 @@
-﻿namespace Tourly.IServices.IUserServices;
+﻿using Tourly.UserModels;
+
+namespace Tourly.IServices.IUserServices;
 public interface IUserService
 {
+    void Register(UserRegisterModel model);
 
+    int Login(UserLoginModel model);
+
+    UserViewModel Get(int id);
+
+    void Update(UserUpdateModel model);
+
+    void Delete(int id);
+
+    List<UserViewModel> GetAll(string search);
+
+    void ChangePassword(int userId, string oldPassword, string newPassword);
 }
---
+++ Tourly/UserModels/UserRegisterModel.cs
@@ -0,0 +1,15 @@
+﻿namespace Tourly.UserModels;
+
+public class UserRegisterModel
+{
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+}
+
+public class UserLoginModel
+{
+    public string PhoneNumber { get; set; }
+    public string Password { get; set; }
+}
